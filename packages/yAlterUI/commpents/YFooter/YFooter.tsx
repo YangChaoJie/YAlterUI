fa054ce5711e5486f8b4aedbc55d919bd57cec05
YangChaoJie <1126913855@qq.com>
2022-01-24 14:49:22 +0800

    commit  add some file

---
+++ packages/yAlterUI/commpents/YFooter/YFooter.tsx
@@ -0,0 +1,16 @@
+import { defineComponent } from "vue";
+
+const YFooter = defineComponent({
+  name: 'YFooter',
+  setup (props, {slots}) {
+    return () => (
+      <div
+      v-slots= {slots}
+      />
+    )
+  }
+})
+
+export {
+  YFooter
+} 
---
+++ packages/yAlterUI/commpents/YFooter/index.ts
@@ -0,0 +1,1 @@
+export { YFooter } from './YFooter'
