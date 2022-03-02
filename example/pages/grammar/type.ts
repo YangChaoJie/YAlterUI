6f6887540b34f96b33abdb86b1d629a838ad1491
YangChaoJie <1126913855@qq.com>
2022-03-02 09:21:59 +0800

    commit fix code

--- example/pages/grammar/type.ts
+++ example/pages/grammar/type.ts
@@ -56,3 +56,153 @@ declare type UnwrapRef<T> = {
 
 const coyb = ref(ref(ref(ref(2))));
 coyb
+const coyy: UnwrapRef<number> = 0
+coyy
+type UnwrapRef0<T> = {
+  ref: T extends Ref<infer R> ? R : T
+  object: {[K in keyof T]: UnwrapRef<T[K]>}
+  other: T
+}[T extends Ref ? 'ref' : T extends object ? 'object' : 'other']
+
+// 索引签名
+/**
+ * 索引签名是 type script 里的一个语法糖
+ * 所谓索引，就是根据一定的指向返回相应的值
+ * typeScript 里的索引签名有两种： 数字牵引和字符串牵引
+ */
+interface Fooo {
+  [key: string]: number
+  x: number
+  y: number // 属性必须为 number 类型
+}
+// 索引签名可以通过映射类型来使索引字符串为联合类型中的一员
+type Index = 'a' | 'b' | 'c'
+type FormIndex = {[k in Index]?: number}
+const good: FormIndex = {a: 1, b: 2};
+
+console.log('good[]', good['a']);
+
+type FormSomeIndex<K extends string> = {[key in K]: number}
+let com : {[code: string]: number} = { 'dd': 1, '': 2 }
+
+interface NestedCSS {
+  color?: string;
+  // [selecor: string]: string | NestedCSS
+  nest?: {
+    [selecor: string]: NestedCSS
+  }
+}
+
+const example: NestedCSS = {
+  color: 'red',
+  nest: {
+    '.subclass': {
+      color: 'blue'
+    }
+  }
+}
+
+// const cvc: NestedCSS = {
+//   colodd: '' 
+// }
+
+// ==========================
+// 条件类型
+// 常见的extends 语句可以分成集中成立情况
+// 字面量类型以及其原始类型
+// 基类与派生类的子类型关系
+// 联合类型与其分支的子类型关系
+// Top Type 与 Bottom Type
+// 分布式 条件类型
+
+// 字面量类型及其原始类型
+
+type Recode = 200 | 400 | 500
+
+interface Foo {
+  status: 'success' | 'failure'
+}
+
+type Minxed = true | 599 | 'dsfsdf'
+
+
+// 子关系类型
+class Base {
+  name!: string;
+}
+
+class Derived extends Base {
+  age!: number
+}
+type _T1 = Derived extends Base ? true : false;
+type _T2 = {name: 'asdfasdfa';} extends Base ? true :  false;
+let bbb: _T2 = true
+
+type _T3 = {name: '12'; age: 8; job: '3edff'} extends Base ? true : false;
+
+let ccc: _T3 = true;
+
+// 联合类型以及其分支 --- 前者是否是后者的子集
+type _T7 = 'a' extends 'a' | 'b' | 'c' ? true : false;
+// true
+type _T8 = 'a' | 'b' extends 'a' | 'b' | 'c' ? true : false;
+// false
+type _T9 = 'a' | 'b' | 'wuhu!' extends 'a' | 'b' | 'c' ? true : false;
+
+
+// Top Type 与 Bottom Type
+/**
+ * 在typeScript 我们说any 与unknown 是Top Type, never则是Bottom Type/
+ * Top Type 意味着它们处于类型层级的顶端，即任意的类型都属于其子类型，对于 OtherType extends any 与 OtherType extends unknown 必定成立。而 Bottom Type 处于类型层级的底端，意味着无法再细分的类型，除了 never 自身，没有别的类型能够再赋值给它。这也就意味着，它属于任意类型的子类型，即 never extends OtherType 必定成立。
+ */
+
+// 8，即所有 extends 均成立
+type _Chain = never extends 'linbudu'
+  ? 'linbudu' extends 'linbudu' | 'budulin'
+    ? 'linbudu' extends string
+      ? string extends {}
+        ? {} extends Object
+          ? Object extends any
+            ? Object extends unknown
+              ? any extends unknown
+                ? unknown extends any
+                  ? 8
+                  : 7
+                : 6
+              : 5
+            : 4
+          : 3
+        : 2
+      : 1
+    : 0
+  : never;
+
+  let ddd : _Chain = 8
+
+  //  分布式条件类型
+
+  type Extract0<T, U> = T extends U ? T : never;
+
+  interface IObject {
+    a: string;
+    b: number;
+    c: boolean;
+  }
+  
+  // 'a'|'b'
+  type _ExtractedKeys1 = Extract<keyof IObject, 'a'|'b'>;
+  
+  // 'a'|'b'
+  type _ExtractedKeys2 = Extract<'a'|'b'|'c', 'a'|'b'>;
+  
+  // never
+  type _ExtractedKeys3 = 'a'|'b'|'c' extends 'a'|'b' ? 'a'|'b'|'c' : never;
+
+  // 交集
+// type Extract<T, U> = T extends U ? T : never;
+
+// 差集
+// type Exclude<T, U> = T extends U ? never : T;
+
+// 移除集合中的 null 与 undefined
+// type NonNullable<T> = T extends null | undefined ? never : T;
