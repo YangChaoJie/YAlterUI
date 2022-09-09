744d4b62735d939faba88be4338e2951d493b52c
YangChaoJie <1126913855@qq.com>
2022-09-09 13:20:16 +0800

    commit  fix  add nanespace

--- packages/yAlterUI/src/components/button/button.tsx
+++ packages/yAlterUI/src/components/button/button.tsx
@@ -3,6 +3,7 @@ import './styles/index.scss'
 
 import { defineComponent, PropType } from "vue";
 import { Size, ButtonType } from "./interface";
+import { useNamespace } from '../../composables/namespace';
 
 const buttonProps = {
   size: String as PropType<Size>,
@@ -22,9 +23,11 @@ const YButton = defineComponent({
   props: buttonProps,
   setup (props, {slots}) {
     const { size, disabled } = props
+    const ns = useNamespace('btn')
     return () => (
       <button
-      class={['btn', `btn-${size}`, {'btn-disabled': disabled }]}
+      // class={['btn', `btn-${size}`, {'btn-disabled': disabled }]}
+      class={[ns.b(), ns.m(size), ns.is('disabled', disabled)]}
       disabled={ disabled || undefined }
       >
         { slots.default?.()}
