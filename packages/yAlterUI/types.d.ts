2700b8e0dedf322a225a9372d281450715e45dec
YangChaoJie <1126913855@qq.com>
2023-03-07 18:23:30 +0800

    FIX: module type path

--- packages/yAlterUI/types.d.ts
+++ packages/yAlterUI/types.d.ts
@@ -1,13 +1,16 @@
 
-declare module 'vue' {
-  export interface GlobalComponents {
-    Button: typeof import('yalert-ui')['YButton'],
-    Icon: typeof import('yalert-ui')['YIcon'],
-    YFooter: typeof import('yalert-ui')['YFooter']
-  }
-  interface ComponentCustomProperties {
+  declare module 'vue' {
+    export interface GlobalComponents {
+      Button: typeof import('yalert-ui')['YButton'],
+Icon: typeof import('yalert-ui')['YIcon'],
+Renderer: typeof import('yalert-ui')['YRenderer'],
+Toast: typeof import('yalert-ui')['YToast'],
+YFooter: typeof import('yalert-ui')['YYFooter']
+    }
 
+    interface ComponentCustomProperties {
+      
+    }
   }
-}
 
-export { }
+  export {}
