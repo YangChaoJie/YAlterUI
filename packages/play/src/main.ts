48f108986845852607f53aff333b6a41293266c4
YangChaoJie <1126913855@qq.com>
2023-03-13 14:12:57 +0800

    feat: add demo

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import  createPlayRouter  from './router'
 import '../../yAlterUI/styles/index.scss'
-// import { install } from 'yalert-ui'
+import { install } from 'yalert-ui'
 console.log('router-----', createPlayRouter);
 const app = createApp(App);
 app.use(createPlayRouter());
-// app.use(install);
+app.use(install);
 app.mount('#app')
