7d65dcdfac302c49c795cb82b76e09de7cc097ad
YangChaoJie <1126913855@qq.com>
2023-04-06 10:58:10 +0800

    feat(play): add pinia

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import  createPlayRouter  from './router'
 import '../../yAlterUI/styles/index.scss'
+import { createPinia } from 'pinia' 
+const pinia = createPinia()
 // 全局引入
 // import { install } from 'yalert-ui'
 import 'uno.css'
 // import '@unocss/reset/antfu.css'
 const app = createApp(App);
+app.use(pinia)
 app.use(createPlayRouter());
 // app.use(install);
 app.mount('#app')
